b6611a0f583089412064fc5fda83256c82704ae0
Donna Hermannsdottir <donna@donnah.is>
2025-04-25 00:16:33 +0000

    feat: simplify obsession auto start

--- nvim/lua/session.lua
+++ nvim/lua/session.lua
@@ -3,18 +3,7 @@ vim.api.nvim_create_augroup("ObsessionAutoStart", { clear = true })
 vim.api.nvim_create_autocmd("VimEnter", {
   group = "ObsessionAutoStart",
   callback = function()
-    -- Only restore session if no files were specified on command line
-    if
-      vim.fn.argc() == 0
-      and vim.v.this_session == ""
-      and vim.fn.filereadable("Session.vim") == 1
-    then
-      vim.cmd("source Session.vim")
-    end
-    -- Start obsession if not already in a session
-    if vim.v.this_session == "" then
-      vim.cmd("silent! Obsession")
-    end
+    vim.cmd("silent! Obsession")
   end,
 })
 
